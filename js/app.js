ca3f28e7c021f2e7bcc38b4cb0da72509abb7edb
Chris <cjheiden227@gmail.com>
2014-08-30 15:52:25 -0700

    Sub Content In

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+$('#magellan').foundation('magellan', {
+  // specify the class used for active sections
+  active_class: 'active',
+  // how many pixels until the magellan bar sticks, 0 = auto
+  threshold: 0,
+  // pixels from the top of destination for it to be considered active
+  destination_threshold: 0,
+  // calculation throttling to increase framerate
+  throttle_delay: 50,
+  // top distance in pixels assigned to the fixed element on scroll
+  fixed_top: 0  
+});
