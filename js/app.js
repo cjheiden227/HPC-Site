17a1e5dd1b41d4e95d4759e0959706bf269cae7c
Chris <cjheiden227@gmail.com>
2014-09-02 08:11:56 -0700

    Footer done

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 $(document).foundation();
 
 $(document).ready(function(){
-    $('.map-link').on('click', function(){
+	
+    $('.map-link').on('click', function(e){
+    	e.preventDefault();
     	address=encodeURIComponent($(this).attr('data-map'));
     	map= $("<iframe height='500' scrolling='no'  marginheight='0' marginwidth='0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyCnXy5N52r2BsyqhjIU-T4shbIqgeaf34g&amp;q="+ address + "'></iframe>");
     	map.addClass('map-target small-12 columns big-icon');
     	$('.map-target').replaceWith(map);
     });
+});
+
+$('.navbarWrapper').stickyNavbar({
+	startAt: 0,
+	selector: "a"
 });
